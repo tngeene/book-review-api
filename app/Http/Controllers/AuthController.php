2d428e20a20301b255e783577ab1f4ad2ff1d135
Champé <tngeene27@gmail.com>
2019-11-24 07:52:29 +0300

    started working with vue.js for front-end

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
-;
 
 class AuthController extends Controller
 {
--- database/migrations/2019_09_29_045047_create_books_table.php
+++ database/migrations/2019_09_29_045047_create_books_table.php
@@ -23,7 +23,7 @@ class CreateBooksTable extends Migration
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
-            
+
     }
 
     /**
